ecb5680567500d4ba46fd89c48051d039655e617
songjunzheng <fengchen692@gmail.com>
2024-04-06 13:10:17 +0800

    项目初始化

---
+++ gulimall-order/src/main/java/com/sjz/gulimall/order/GulimallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.sjz.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallOrderApplication.class, args);
+    }
+
+}
